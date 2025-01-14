252de03538452426fdcb5c603ae59f74587038c6
JASH GUSANI~ <jashgusani20@gmail.com>
2025-01-14 11:11:50 +0530

    Initial Commite

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Route,BrowserRouter as Router, Routes } from 'react-router-dom'
+import './App.css'
+import Main from './Components/Main'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Main />}></Route>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App

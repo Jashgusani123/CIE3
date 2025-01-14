24f761aeb691b28e0b9af2ab1cd8d33b11c060ce
JASH GUSANI~ <jashgusani20@gmail.com>
2025-01-14 11:25:48 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Route,BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
-import Main from './Components/Home'
 import Navbar from './Components/NavBar'
 import Home from './Components/Home'
 
